b6133017f8d171ce734bb7cb2a7cb43c9f36605e
李倩如 <876794103@qq.com>
2024-03-22 15:02:34 +0800

    feat: galaxy-完成

--- src/lessons/GalaxyGenerator.vue
+++ src/lessons/GalaxyGenerator.vue
@@ -33,23 +33,71 @@ const initFunction = () => {
 
   const control = new OrbitControls(camera, renderer.domElement);
 
+  let points = null,
+    positions = null,
+    pointMesh = null,
+    insideColor = new THREE.Color('#ff6030'),
+    outsideColor = new THREE.Color('#1b3984'),
+    colors = null;
+
   const generatorGalaxy = () => {
-    const points = new THREE.BufferGeometry();
-    const count = 1000;
-    const positions = new Float32Array(count * 3);
+    points = new THREE.BufferGeometry();
+    const count = 100000;
+    const branchs = 4;
+    const radius = 5;
+    const spin = 1;
+    const randomness = 0.2;
+    const randomPow = 3;
+
+    positions = new Float32Array(count * 3);
+    colors = new Float32Array(count * 3);
+
     for (let i = 0; i < count; i++) {
       const i3 = i * 3;
-      positions[i3] = (Math.random() - 0.5) * 4;
-      positions[i3 + 1] = (Math.random() - 0.5) * 4;
-      positions[i3 + 2] = (Math.random() - 0.5) * 4;
+      const branchAngle = ((i % branchs) / branchs) * 2 * Math.PI;
+      const randomRadius = Math.random() * radius;
+      const randomSpin = randomRadius * spin;
+      const randomX =
+        Math.pow(Math.random(), randomPow) *
+        randomness *
+        randomRadius *
+        (Math.random() < 0.5 ? -1 : 1);
+      const randomY =
+        Math.pow(Math.random(), randomPow) *
+        randomness *
+        randomRadius *
+        (Math.random() < 0.5 ? -1 : 1);
+      const randomZ =
+        Math.pow(Math.random(), randomPow) *
+        randomness *
+        randomRadius *
+        (Math.random() < 0.5 ? -1 : 1);
+
+      positions[i3] =
+        Math.sin(branchAngle + randomSpin) * randomRadius + randomX;
+      positions[i3 + 1] = randomY;
+      positions[i3 + 2] =
+        Math.cos(branchAngle + randomSpin) * randomRadius + randomZ;
+
+      // 生成随机颜色
+      const mixedColor = insideColor.clone();
+      mixedColor.lerp(outsideColor, randomRadius / radius);
+
+      colors[i3] = mixedColor.r;
+      colors[i3 + 1] = mixedColor.g;
+      colors[i3 + 2] = mixedColor.b;
     }
+
     points.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-    const pointMesh = new THREE.Points(
+    points.setAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+    pointMesh = new THREE.Points(
       points,
       new THREE.PointsMaterial({
-        size: 0.02,
+        size: 0.01,
         depthWrite: false,
         blending: THREE.AdditiveBlending,
+        vertexColors: true,
       })
     );
     scene.add(pointMesh);
